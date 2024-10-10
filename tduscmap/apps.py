d3b51ad56c232f5cc61b5f86d303315556c301a2
canofranck <cano.franck77@gmail.com>
2024-10-10 19:09:46 +0200

    test deploy

---
+++ tduscmap/apps.py
@@ -0,0 +1,11 @@
+
+from django.apps import AppConfig
+
+
+class OCLettingsSiteConfig(AppConfig):
+    """Configuration class for the 'lettings' application.
+
+    This class configures the 'oc_lettings_site' application, which handles
+    functionalities related to properties in the Django project.
+    """
+    name = 'tduscmap'
