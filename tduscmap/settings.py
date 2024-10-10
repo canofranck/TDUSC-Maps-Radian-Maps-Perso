4bce866bb7b4c8977b097fa893a189f75c1b5eb7
canofranck <cano.franck77@gmail.com>
2024-10-10 19:40:18 +0200

    test deploy ok

--- tduscmap/settings.py
+++ tduscmap/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 # Application definition
 
 INSTALLED_APPS = [
-    'tduscmap.apps.tduscmapConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
