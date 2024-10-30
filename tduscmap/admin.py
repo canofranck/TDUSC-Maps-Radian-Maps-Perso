a683525617e475932a5c6ba1786f71b1f635f666
canofranck <cano.franck77@gmail.com>
2024-10-30 10:17:29 +0100

    reglage voiture en cours

--- tduscmap/admin.py
+++ tduscmap/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Car, CarPrice
+from .models import Car, CarPrice, Reglage, Like, ConfigurationReglage
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
@@ -7,8 +10,77 @@ class CarAdmin(admin.ModelAdmin):
     list_filter = ("marque", "transmission", "categorie", "rarete")
     search_fields = ("marque", "modele")
 
+
 @admin.register(CarPrice)
 class CarPriceAdmin(admin.ModelAdmin):
     list_display = ("car", "prix", "date")
     list_filter = ("car__marque",)
     search_fields = ("car__marque", "car__modele")
+
+
+class ReglageAdminForm(ModelForm):
+    class Meta:
+        model = Reglage
+        fields = '__all__'
+
+    
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        
+        # Assurez-vous que 'voiture' est bien défini
+        if instance.voiture:
+            configuration, created = ConfigurationReglage.objects.get_or_create(car=instance.voiture)
+            instance.configurationreglage = configuration
+        else:
+            raise ValidationError("La voiture doit être sélectionnée.")
+
+        if commit:
+            instance.save()
+        return instance
+
+@admin.register(Reglage)
+class ReglageAdmin(admin.ModelAdmin):
+    form = ReglageAdminForm
+    # Ici, vous pouvez personnaliser l'affichage dans l'admin pour Reglage
+    # list_display = ('voiture', 'utilisateur', 'durete_ressorts_arriere', 'durete_ressorts_avant')  # Exemple
+    list_display = [field.name for field in Reglage._meta.fields]
+    list_filter = ('voiture__marque',)
+    search_fields = ('voiture__modele',)
+    fieldsets = [
+        ('Informations générales', {
+            'fields': ('voiture', 'utilisateur', 'description')
+        }),
+        ('Boîte de vitesse', {
+            'fields': ('rapport_final', 'premiere_vitesse', 'deuxieme_vitesse', 'troisieme_vitesse', 'quatrieme_vitesse', 'cinquieme_vitesse', 'sixieme_vitesse', 'septieme_vitesse')
+        }),
+        ('Aérodynamisme', {
+            'fields': ('appui_aerodynamique_avant', 'appui_aerodynamique_arriere')
+        }),
+        ('Suspension et ressorts', {
+            'fields': ('taille_suspension_arriere', 'taille_suspension_avant', 'durete_ressorts_arriere', 'durete_ressorts_avant')
+        }),
+        ('Amortisseurs', {
+            'fields': ('compression_amortisseurs_avant', 'compression_amortisseurs_arriere', 'decompression_amortisseurs_avant', 'decompression_amortisseurs_arriere')
+        }),
+        ('Conduite sportive', {
+            'fields': ('differentiel_arriere', 'acceleration_arriere', 'deceleration_arriere', 'freinage_arriere', 'durete_barre_antiroulis_avant', 'durete_barre_antiroulis_arriere', 'angle_carrossage_arriere', 'angle_carrossage_avant', 'pression_pneus_arriere', 'pression_pneus_avant')
+        }),
+        ('Freinage', {
+            'fields': ('repartiteur_freinage_avant', 'pression_freinage')
+        })
+    ]
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('voiture', 'configurationreglage')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('reglage', 'user', 'created_at')
+
+@admin.register(ConfigurationReglage)
+class ConfigurationReglageAdmin(admin.ModelAdmin):
+    list_display = ( 'car', 'rapport_final_min', 'rapport_final_max')  # Champs à afficher dans la liste
+    search_fields = [ 'car']  # Champs pour la recherche
+    # Ajoutez d'autres options de personnalisation si nécessaire (filtres, tri, etc.)
--- tduscmap/models.py
+++ tduscmap/models.py
@@ -1,8 +1,9 @@
 
 from django.db import models
+from django.forms import ValidationError
 from django.utils import timezone
 from authentication.models import CustomUser
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Favorite(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
@@ -53,7 +54,7 @@ class Car(models.Model):
         ('FWD', 'FWD'),
         ('4WD', '4WD'),
     ]
-    
+
     CATEGORIES = [
         ('A', 'Catégorie A'),
         ('B', 'Catégorie B'),
@@ -64,7 +65,7 @@ class Car(models.Model):
         ('G', 'Catégorie G'),
         ('H', 'Catégorie H'),
     ]
-    
+
     RARETES = [
         ('légendaire', 'Légendaire'),
         ('épique', 'Épique'),
@@ -73,12 +74,11 @@ class Car(models.Model):
         ('peu commun', 'Peu commun'),
         (None, 'Aucune'),
     ]
-    
+
     marque = models.CharField(max_length=50, choices=MARQUES)
     modele = models.CharField(max_length=50)
     annee = models.PositiveIntegerField()
     transmission = models.CharField(max_length=3, choices=TRANSMISSIONS)
-    
     ip = models.PositiveIntegerField()
     categorie = models.CharField(max_length=1, choices=CATEGORIES)
     rarete = models.CharField(max_length=10, choices=RARETES, blank=True, null=True)
@@ -92,6 +92,139 @@ class CarPrice(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="historique_prix")
     prix = models.PositiveIntegerField()
     date = models.DateField(default=timezone.now)
-    
+
     def __str__(self):
         return f"{self.car.marque} {self.car.modele} - {self.date}: {self.prix}€"
+
+
+class ConfigurationReglage(models.Model):
+    # reglage = models.OneToOneField('Reglage', on_delete=models.CASCADE)  # Lien vers le modèle Reglage
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    # Champs spécifiques pour la configuration
+    rapport_final_min = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    rapport_final_max = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    premiere_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    premiere_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    deuxieme_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    deuxieme_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    troisieme_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    troisieme_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    quatrieme_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    quatrieme_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    cinquieme_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    cinquieme_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    sixieme_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    sixieme_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    septieme_vitesse_min = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    septieme_vitesse_max = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    appui_aerodynamique_avant_min = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    appui_aerodynamique_avant_max = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    appui_aerodynamique_arriere_min = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    appui_aerodynamique_arriere_max = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+
+
+class Reglage(models.Model):
+    """Modèle pour stocker les réglages d'une voiture"""
+    voiture = models.ForeignKey(Car, on_delete=models.CASCADE)
+    utilisateur = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    configurationreglage =  models.ForeignKey(ConfigurationReglage, on_delete=models.CASCADE)
+    # Boîte de vitesse
+    rapport_final = models.DecimalField(max_digits=5, decimal_places=2)
+    premiere_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+    deuxieme_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+    troisieme_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+    quatrieme_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+    cinquieme_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+    sixieme_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+    septieme_vitesse = models.DecimalField(max_digits=4, decimal_places=2)
+
+    # Aérodynamisme
+    appui_aerodynamique_avant = models.DecimalField(max_digits=3, decimal_places=1)
+    appui_aerodynamique_arriere = models.DecimalField(max_digits=3, decimal_places=1)
+
+    # Stabilité
+    taille_suspension_arriere = models.DecimalField(max_digits=4, decimal_places=2, validators=[MinValueValidator(0.11), MaxValueValidator(0.16)])
+    taille_suspension_avant = models.DecimalField(max_digits=4, decimal_places=2, validators=[MinValueValidator(0.11), MaxValueValidator(0.16)])
+
+    # Ressort
+    durete_ressorts_arriere = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.8), MaxValueValidator(1.2)])
+    durete_ressorts_avant = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.8), MaxValueValidator(1.2)])
+
+    # Amortisseurs
+    compression_amortisseurs_avant = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.8), MaxValueValidator(1.2)])
+    compression_amortisseurs_arriere = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.8), MaxValueValidator(1.2)])
+    decompression_amortisseurs_avant = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.8), MaxValueValidator(1.2)])
+    decompression_amortisseurs_arriere = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.8), MaxValueValidator(1.2)])
+
+    # Conduite sportive
+    differentiel_arriere = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    acceleration_arriere = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    deceleration_arriere = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    freinage_arriere = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    durete_barre_antiroulis_avant = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    durete_barre_antiroulis_arriere = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    angle_carrossage_arriere = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(-6), MaxValueValidator(3)])
+    angle_carrossage_avant = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(-6), MaxValueValidator(3)])
+    pression_pneus_arriere = models.DecimalField(max_digits=2, decimal_places=1, validators=[MinValueValidator(2), MaxValueValidator(6)])
+    pression_pneus_avant = models.DecimalField(max_digits=2, decimal_places=1, validators=[MinValueValidator(2), MaxValueValidator(6)])
+
+    # Freinage
+    repartiteur_freinage_avant = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    pression_freinage = models.IntegerField(validators=[MinValueValidator(-1000), MaxValueValidator(1000)])
+
+    def __str__(self):
+        return f"Réglage de {self.utilisateur} pour {self.voiture}"
+
+    def get_configuration(self):
+        return ConfigurationReglage.objects.get(car=self.voiture)
+    
+    def clean_rapport_final(self):
+        """Validation de rapport_final par rapport aux min et max de configurationreglage."""
+        if self.configurationreglage is None:
+            raise ValidationError("Aucune configurationreglage associée.")
+        config = self.configurationreglage
+        errors = {}
+        # Afficher les valeurs pour déboguer
+        print("Validation de rapport_final:")
+        print(f"Valeur actuelle : {self.rapport_final}")
+        print(f"Min : {config.rapport_final_min}, Max : {config.rapport_final_max}")
+        # Exemple pour la validation du champ rapport_final
+        if config.rapport_final_min is not None and config.rapport_final_max is not None:
+            if self.rapport_final < config.rapport_final_min or self.rapport_final > config.rapport_final_max:
+                errors['rapport_final'] = f"La valeur de 'rapport_final' doit être comprise entre {config.rapport_final_min} et {config.rapport_final_max}."
+
+        # Validation pour premiere_vitesse
+        if config.premiere_vitesse_min is not None and config.premiere_vitesse_max is not None:
+            print("Validation de premiere_vitesse:")
+            print(f"Valeur actuelle : {self.premiere_vitesse}")
+            print(f"Min : {config.premiere_vitesse_min}, Max : {config.premiere_vitesse_max}")
+            if self.premiere_vitesse < config.premiere_vitesse_min or self.premiere_vitesse > config.premiere_vitesse_max:
+                errors['premiere_vitesse'] = f"La valeur de 'premiere_vitesse' doit être comprise entre {config.premiere_vitesse_min} et {config.premiere_vitesse_max}."
+
+        # Ajoutez ici les autres validations de champ (deuxieme_vitesse, troisieme_vitesse, etc.)
+
+        # Lève une ValidationError si des erreurs ont été collectées
+        if errors:
+            raise ValidationError(errors)
+        
+    # def clean(self):
+    #    # Ajoutez un print pour déboguer
+    #     print(f"ConfigurationReglage de {self} : {self.configurationreglage}")
+    #     super().clean()
+        
+    def save(self, *args, **kwargs):
+        # Appel de clean() pour effectuer la validation avant de sauvegarder
+        self.clean()
+        super().save(*args, **kwargs)
+
+
+class Like(models.Model):
+    reglage = models.ForeignKey(Reglage, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        unique_together = ('reglage', 'user')  # Ensure a user can only like a réglage once
