db0b271014fbcf1e9458b8f33e948464563f6418
canofranck <cano.franck77@gmail.com>
2024-12-17 10:59:30 +0100

    ajout secu localhost

--- authentication/forms.py
+++ authentication/forms.py
@@ -7,3 +7,10 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if username.lower() == "localhost@localhost.localhost".lower():
+            raise forms.ValidationError("Ce nom d'utilisateur n'est pas autorisé.")
+        if "localhost" in username.lower():
+            raise forms.ValidationError("Le mot 'localhost' n'est pas autorisé dans le nom d'utilisateur.")
+        return username
